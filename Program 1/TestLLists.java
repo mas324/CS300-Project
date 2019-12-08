7d91e8b597c7b0d464f7bf50fc95ae3ac88f8eef
Shawn Merana <20176147+mas324@users.noreply.github.com>
2019-12-07 18:47:53 -0800

    Update TestLLists.java
    Put in a remove tester

--- Program 1/TestLLists.java
+++ Program 1/TestLLists.java
@@ -4,39 +4,52 @@ public class TestLLists
 	public static void main(String [] args) 
 	{
 		final int NUM_DATA = 100000;
-		DLList <Integer>linkList = new DLList<Integer>(); 
-		DLList <String>strLList = new DLList<String>(); 
+		DLList <Integer> intList = new DLList<Integer>(); 
+		DLList <String> strLList = new DLList<String>(); 
 		
 		// First we will be inserting in the front
 		//First for the Doubly Linked List
 		long start = System.currentTimeMillis(); //for the DLList
 		for (int i = 0; i < NUM_DATA; i++)
-			linkList.add(0,i);
+			intList.add(0, i);
 		long elapsed = System.currentTimeMillis() - start;
 		System.out.printf("Doubly Linked List when adding to front: %d ms\n", elapsed);
 		
 		//Now for the Doubly Linked List of type String
-		start = System.currentTimeMillis(); // DLL
+		start = System.currentTimeMillis();
 		for (int i = 0; i < NUM_DATA; i++)
-			strLList.add(String.valueOf((char)('a' + i)));
+			strLList.add(0, String.valueOf((char)('a' + i)));
 		elapsed = System.currentTimeMillis() - start;
 		System.out.printf("Doubly Linked List of type String when adding to front: %d ms\n", elapsed);
 
 		//Now we are inserting at the rear 
 		//First the Doubly Linked List
-		linkList.clear();
+		intList.clear();
 		start = System.currentTimeMillis();
 		for (int i = 0; i < NUM_DATA; i++)
-			linkList.add(i);
+			intList.add(Integer.valueOf(i));
 		elapsed = System.currentTimeMillis() - start;
 		System.out.printf("Doubly Linked List when adding to rear: %d ms\n", elapsed);
 		
 		//Now the Doubly Linked List of type String
 		strLList.clear();
-		start = System.currentTimeMillis(); // ArrayList
+		start = System.currentTimeMillis();
 		for (int i = 0; i < NUM_DATA; i++)
 			strLList.add(String.valueOf((char)('a' + i)));
 		elapsed = System.currentTimeMillis() - start;
 		System.out.printf("Doubly Linked List of type String when adding to rear: %d ms\n", elapsed);
+
+		//Testing the remove method
+		start = System.currentTimeMillis();
+		for (int i = 0; i < intList.size() - 1; i++)
+			intList.remove(i);
+		elapsed = System.currentTimeMillis();
+		System.out.printf("It took %d ms to remove all items\n", elapsed);
+
+		start = System.currentTimeMillis();
+		for (int i = 0; i < strLList.size() - 1; i++)
+			strLList.remove(i);
+		elapsed = System.currentTimeMillis();
+		System.out.printf("It took %d ms to remove all items\n", elapsed);
 	}
 }
