c5771e8b80c446b70c1962332543a62f1ba9e508
Leslie Jaimes <58496441+lesliesamantha@users.noreply.github.com>
2019-12-09 14:39:50 -0800

    Update Visitor.java

---
+++ Program 3/Visitor.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+public class Visitor {
+
+	private int timeOntoQueue;
+	private int timeOutOfQueue;
+	private int totalTimeInQueue;
+	private int timeInGallery;
+	private int timeRemainingInGallery;
+	private static int randy; 
+	
+	public Visitor() {
+		//constructor for Visitor
+	}
+
+	void setTimeOntoQueue(int timeOntoQueue){
+	//method
+	}
+	void setTimeOutOfQueue(int timeOutOfQueue){
+		//method
+	}
+	void setTimeInGallery() {
+		//method
+	}
+	getTotalTimeInQueue(){
+		//getter
+	}
+	getTimeRemainingInGallery(){
+		//getter
+	}
+
+}
+
