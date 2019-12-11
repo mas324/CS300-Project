cec2aea44335e96d25b60f64894db6a4461b5e61
Shawn Merana <masterman324@gmail.com>
2019-12-11 00:17:37 -0800

    New visitors
    Tidy up visitor class
    Added Regular and VIP visits

--- Program 3/Visitor.java
+++ Program 3/Visitor.java
@@ -1,48 +1,50 @@
 import java.util.Random;
+
 public class Visitor {
 
-	private int timeOntoQueue;
-	private int timeOutOfQueue;
-	private int totalTimeInQueue;
-	private int timeInGallery;
-	private int timeRemainingInGallery;
+	private int timeOntoQueue, timeOutOfQueue, totalTimeInQueue, timeInGallery, timeRemainingInGallery;
 	private static Random randy = new Random(2);
-	private int randomP;
-	
+
 	public Visitor(int time) {
 		setTimeOntoQueue(time);
 		setTimeInGallery();
 	}
-	public void setTimeOntoQueue(int timeOntoQueue){
+
+	public void setTimeOntoQueue(int timeOntoQueue) {
 		this.timeOntoQueue = timeOntoQueue;
 	}
-	public void setTimeOutOfQueue(int timeOutOfQueue){
+
+	public void setTimeOutOfQueue(int timeOutOfQueue) {
 		this.timeOutOfQueue = timeOutOfQueue;
 	}
+
 	public void setTotalTimeInQueue() {
 		totalTimeInQueue = timeOutOfQueue - timeOntoQueue;
 	}
+
 	public void setTimeInGallery() {
-		randomP = randy.nextInt(100);
-		
-		if(randomP <= 20) 
+		int randomP = randy.nextInt(100) + 1;
+
+		if (randomP <= 20)
 			timeInGallery = 5;
-		else if(randomP <= 40) 
+		else if (randomP <= 40)
 			timeInGallery = 20;
-		else 
+		else
 			timeInGallery = 10;
 
 		timeRemainingInGallery = timeInGallery;
 	}
+
 	public void decrementTimeRemainingInGallery() {
 		timeRemainingInGallery--;
 	}
-	public int getTotalTimeInQueue(){
+
+	public int getTotalTimeInQueue() {
 		return totalTimeInQueue;
 	}
-	public int getTimeRemainingInGallery(){
+
+	public int getTimeRemainingInGallery() {
 		return timeRemainingInGallery;
 	}
 
 }
-
