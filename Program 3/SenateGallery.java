df0271568dba5e864b7335c0a1cbc64c1d9ad7f4
Shawn Merana <masterman324@gmail.com>
2019-12-11 12:20:19 -0800

    Update SenateGallery.java

--- Program 3/SenateGallery.java
+++ Program 3/SenateGallery.java
@@ -1,25 +1,26 @@
 
 public class SenateGallery {
-	
-		public static void initializeSenateGallery()
-		{ 
-		
-		}
-		
-		
-		
-		public static void runSenateGallerySimulation(int simMinutes) 
-		{ 
-			
-			
-		} 
-	
-		public static void ouputStatistics()
-		{ 
-			
-		} 
-		
-		
+	String date;
+	Dequeue<Visitor> waitingLine;
+	DLList<Visitor> visitorInGallery, visitorFinished;
 
+	public SenateGallery(String date) {
+		this.date = date;
+		waitingLine = new Dequeue<Visitor>(Short.MAX_VALUE);
+		visitorFinished = new DLList<Visitor>();
+		visitorInGallery = new DLList<Visitor>();
+	}
+
+	public void initializeSenateGallery() {
+
+	}
+
+	public void runSenateGallerySimulation(int simMinutes) {
+
+	}
+
+	public void ouputStatistics() {
+
+	}
 
 }
