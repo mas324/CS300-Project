42258c3a9c159bf7cd5bd745e3ea57787f7e0794
icastellanosmaravil <58501632+icastellanosmaravil@users.noreply.github.com>
2019-12-11 06:37:59 -0800

    Add files via upload

---
+++ Program 3/CreateGallerySimulation.java
@@ -0,0 +1,17 @@
+
+
+public class CreateGallerySimulation {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Please enter the Month, day and year of the simulation: ");
+		System.out.println("Please enter the length of the simulation in minutes");
+		System.out.println("The date to the visit to the Senate Gallery is: "); 
+		System.out.println("The people who finished viewing is: "); 
+		System.out.println("The people who still are in gallery is: "); 
+		System.out.println("The total of people who are still waiting: "); 
+		System.out.println("The average time for VIP visitors who are in the gallery or finished viewing is: ");
+		System.out.println("The average wait time for Regular Visitors who are in the gallery or finished viewing is: ");
+		
+	}
+}
