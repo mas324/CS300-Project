31fe5741be6df8002cfff4dc4e1de0e3ae34dea1
Shawn Merana <masterman324@gmail.com>
2019-12-12 00:33:55 -0800

    Fixed driver class

--- Program 3/CreateGallerySimulation.java
+++ Program 3/CreateGallerySimulation.java
@@ -1,15 +1,14 @@
-
+import java.util.Scanner;
 
 public class CreateGallerySimulation {
-
-	public static void main(String[] args) 
-	{
-		System.out.println("Please enter the Month, day and year of the simulation: ");
-		System.out.println("Please enter the length of the simulation in minutes");
-		System.out.println("The date to the visit to the Senate Gallery is: "); 
-		System.out.println("The people who finished viewing is: "); 
-		System.out.println("The people who still are in gallery is: "); 
-		System.out.println("The total of people who are still waiting: "); 
-		
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter the Month Day and Year of the simulation: ");
+		SenateGallery gallery = new SenateGallery(input.nextLine());
+		gallery.initializeSenateGallery();
+		System.out.print("Please enter the length of the simulation in minutes: ");
+		gallery.runSenateGallerySimulation(input.nextInt());
+		input.close();
+		gallery.ouputStatistics();
 	}
 }
